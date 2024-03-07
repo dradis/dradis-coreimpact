97c58f0f8f22504d41155be915d224863bc158cb
Matt Budz <matt.budz@protonmail.com>
2024-03-07 17:23:51 +0100

    add default mappings

--- lib/dradis/plugins/coreimpact/mapping.rb
+++ lib/dradis/plugins/coreimpact/mapping.rb
@@ -1,6 +1,22 @@
 module Dradis::Plugins::Coreimpact
   module Mapping
    def self.default_mapping
+    {
+      'evidence' => {
+        'AgentDeployed' => '{{ coreimpact[evidence.agent_deployed] }}',
+        'Description' => '{{ coreimpact[evidence.description] }}',
+        'Port' => '{{ coreimpact[evidence.port] }}',
+        'TriedToInstallAgent' => '{{ coreimpact[evidence.tried_to_install_agent] }}'
+      },
+      'issue' => {
+        'Title' => '{{ coreimpact[issue.title] }}',
+        'AgentDeployed' => '{{ coreimpact[issue.agent_deployed] }}',
+        'CVE' => '{{ coreimpact[issue.cve] }}',
+        'Description' => '{{ coreimpact[issue.description] }}',
+        'Port' => '{{ coreimpact[issue.port] }}',
+        'TriedToInstallAgent' => '{{ coreimpact[issue.tried_to_install_agent] }}'
+      }
+    }
    end
   end
 end
