6883dcf1db8829f46010b33822c8074a46c0480e
Matt Budz <matt.budz@protonmail.com>
2024-03-18 19:19:13 +0100

    use constant for default mappings

--- lib/dradis/plugins/coreimpact/mapping.rb
+++ lib/dradis/plugins/coreimpact/mapping.rb
@@ -1,14 +1,13 @@
 module Dradis::Plugins::Coreimpact
   module Mapping
-   def self.default_mapping
-    {
-      'evidence' => {
+    DEFAULT_MAPPING = {
+      evidence: {
         'AgentDeployed' => '{{ coreimpact[evidence.agent_deployed] }}',
         'Description' => '{{ coreimpact[evidence.description] }}',
         'Port' => '{{ coreimpact[evidence.port] }}',
         'TriedToInstallAgent' => '{{ coreimpact[evidence.tried_to_install_agent] }}'
       },
-      'issue' => {
+      issue: {
         'Title' => '{{ coreimpact[issue.title] }}',
         'AgentDeployed' => '{{ coreimpact[issue.agent_deployed] }}',
         'CVE' => '{{ coreimpact[issue.cve] }}',
@@ -16,7 +15,6 @@ module Dradis::Plugins::Coreimpact
         'Port' => '{{ coreimpact[issue.port] }}',
         'TriedToInstallAgent' => '{{ coreimpact[issue.tried_to_install_agent] }}'
       }
-    }
-   end
+    }.freeze
   end
 end
